56cd41beb5a019ab9dccb3dcb0e13c4bc706b6ec
Victor Martinez <vcrmartinez@gmail.com>
2020-12-16 19:20:58 -0300

    feat: render step form

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React from "react"
 
-import Container from "../../components/Container";
+import Container from "../../components/Container"
+import StepForm from "../../components/StepForm"
 
-const Home = () => <Container>Home!</Container>;
+const Home = () => (
+  <Container>
+    Home! <StepForm />
+  </Container>
+)
 
-export default Home;
+export default Home
