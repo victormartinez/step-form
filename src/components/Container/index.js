3923e3964048f9bc73d266b668b1c1edc220791f
Victor Martinez <vcrmartinez@gmail.com>
2020-12-16 19:10:59 -0300

    style: prettier

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from "react"
 
-import * as S from "./styled";
+import * as S from "./styled"
 
 const Container = ({ children }) => (
   <S.Container>
     <S.Main>{children}</S.Main>
   </S.Container>
-);
+)
 
-export default Container;
+export default Container
